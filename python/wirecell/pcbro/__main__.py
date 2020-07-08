f639c6766d850858191def1e8733d92290c2fea7
Brett Viren <brett.viren@gmail.com>
2020-07-08 16:03:14 -0400

    Quick-and-dirty reduction with slightly less quick-and-dirty multi event scanner (multipage PDF)

--- python/wirecell/pcbro/__main__.py
+++ python/wirecell/pcbro/__main__.py
@@ -107,6 +107,66 @@ def plot_one(tag, trigger, aspect, output, npzfile):
     plt.tight_layout()
     plt.savefig(output, bbox_inches='tight')
 
+@cli.command("plot-many")
+@click.option("-a","--aspect", default=1.0,
+              help="Aspect ratio")
+@click.option("-o","--output",default="plot.pdf",
+              help="Output file")
+@click.argument("npzfile")
+def plot_many(aspect, output, npzfile):
+    '''
+    Plot waveforms of a trigger from file
+    '''
+    import numpy
+    import matplotlib.pyplot as plt 
+    from matplotlib.backends.backend_pdf import PdfPages
+    fp = numpy.load(npzfile)
+    print (list(fp.keys()))
+
+    with PdfPages(output) as pdf:
+        for k in fp.keys():
+            print (k)
+            if not k.startswith("frame_"):
+                continue;
+            a = fp[k]
+            rows, cols = a.shape;
+            # a = numpy.flip(a,0)
+            fig, ax = plt.subplots(nrows=1, ncols=1)
+            plt.imshow(a, aspect=aspect, interpolation=None)
+            plt.plot((cols//2, cols//2), (0, rows-1), linewidth=0.1, color='red')
+            c = plt.colorbar()
+            plt.gca().invert_yaxis()
+            plt.tight_layout()
+            pdf.savefig(fig, bbox_inches='tight')
+            plt.close()
+
+
+@cli.command("activity")
+@click.option("-t", "--threshold", default=5000,
+             help="Threshold on the sum of values above minimum")
+@click.option("-m", "--minimum", default=5,
+             help="Minimum sample value to be included in sum")
+@click.argument("npzfile")
+@click.argument("npzout")
+def activity(tag, trigger, npzfile, npzout):
+    import numpy
+    fp = numpy.load(npzfile)
+    arrays = dict()
+    for k in fp.keys():
+        if not k.startswith("frame_"):
+            continue
+        a = fp[k]
+        rows,cols = a.shape
+        c = a[:,:cols//2]
+        cn = numpy.array(c - numpy.median(c, axis=0))
+        act = numpy.sum(cn[cn>5])
+        if act < 5000.0:
+            continue
+        arrays[k] = a
+        print (f'save {k}')
+    numpy.savez(npzout, **arrays)
+        
+
 def main():
     cli(obj=dict())
 
